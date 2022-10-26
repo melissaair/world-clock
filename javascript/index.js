ee4edb63e57720c5e99a1e06e378d15be6cfb15f
melissaair <100801990+melissaair@users.noreply.github.com>
2022-10-26 01:01:23 -0700

    Added current timezone option

--- javascript/index.js
+++ javascript/index.js
@@ -45,6 +45,9 @@ function updateTime() {
 
 function updateCity(event) {
 	let cityTimeZone = event.target.value;
+	if (cityTimeZone === "current") {
+		cityTimeZone = moment.tz.guess();
+	}
 	let cityName = cityTimeZone.replace("_", " ").split("/")[1];
 	let cityTime = moment().tz(cityTimeZone);
 	let citiesElement = document.querySelector("#cities");
@@ -54,10 +57,11 @@ function updateCity(event) {
 			<h2>${cityName}</h2>
 			<div class="date">${cityTime.format("dddd MMMM Do, YYYY")}</div>
 		</div>
-		<div class="time">${cityTime.format("h:mm:ss")}<small>${cityTime.format(
+		<div class="time">${cityTime.format("h:mm")}<small>${cityTime.format(
 		"A"
 	)}</small></div>
 	</div>
+    <a href="/">View all cities</a>
     `;
 }
 
